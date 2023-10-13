3984e4c5b117762b11ac0113f7afc5571a908221
Mateus Alves <mateusalves112@gmail.com>
2023-10-13 10:58:59 -0300

    ToDoList Backend on Spring

--- src/main/java/com/example/todo/todolist/users/IUsersDTO.java
+++ src/main/java/com/example/todo/todolist/users/IUsersDTO.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface IUsersDTO extends JpaRepository<User, UUID> {
+   User findByUserName(String username);
 }
--- src/main/java/com/example/todo/todolist/users/UserController.java
+++ src/main/java/com/example/todo/todolist/users/UserController.java
@@ -1,6 +1,8 @@
 package com.example.todo.todolist.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,8 +11,12 @@ public class UserController {
     @Autowired
     private IUsersDTO userRepository;
     @PostMapping("/")
-    public void createUser(@RequestBody User userModel){
+    public ResponseEntity create(@RequestBody User userModel){
+        var users = this.userRepository.findByUserName(userModel.getName());
+        if (users != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
         var userCreated = this.userRepository.save(userModel);
-
+    return ResponseEntity.status(HttpStatus.CREATED).body("Usuário Criado");
     }
 }
